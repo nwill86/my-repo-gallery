3a83e7dd64ec4e6ac43cfa5fcf0214225671d1fb
NATASHA WILLIAMS <147554213+nwill86@users.noreply.github.com>
2024-01-10 16:08:59 -0500

    Fetched repo data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // Div overview (profile information)
 const overviewElement = document.querySelector(".overview");
+const repoElement = document.querySelector(".repo-list");
+
 const username = "nwill86";
 
 const gitUserData = async function () {
@@ -23,4 +25,22 @@ const showUserInfo = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`
     overviewElement.append(div);
+    gitUserRepos();
 };
+
+const gitUserRepos = async function () {
+    const repos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await repos.json();
+    showUserRepos(repoData);
+}
+
+const showUserRepos = function (repos) {
+    for (let repo of repos) {
+        const div = document.createElement("div");
+        div.classList.add("repo");
+        div.innerHTML = `<h3>${repo.name}</h3>`;
+        repoElement.append(div);
+    }
+};
+
+
